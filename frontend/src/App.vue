7bc21ef4dd3008c06f863f4bfce646f459a8f057
Ruba <nathan.ruba2003@gmail.com>
2024-02-28 20:27:30 -0300

    PrimeFlex installation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+   import HeaderVue from "./components/HeaderVue.vue"
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
+<template>
+  <link rel="stylesheet" href="https://unpkg.com/primeflex@latest/primeflex.css">
+  
+  <HeaderVue></HeaderVue>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import CustomButton from "primevue/button"
+import "/node_modules/primeflex/primeflex.css"
+import "primeflex/themes/primeone-light.css"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('CustomButton', CustomButton)
+app.mount('#app')
