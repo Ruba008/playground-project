aad66a3cd97a30ce026b484fc703b4209fd0f429
Ruba <nathan.ruba2003@gmail.com>
2024-02-29 20:47:11 -0300

    Defining backend

--- frontend/src/components/HashGame.vue
+++ frontend/src/components/HashGame.vue
@@ -3,16 +3,16 @@
 
 <template>
     <div class="flex w-full justify-center">
-        <div class="grid grid-cols-3 text-center w-60 h-60">
-            <div class="flex cursor-pointer items-center justify-center border-r-4 border-b-4 border-black">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-r-4 border-b-4 border-black">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black border-b-4">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black border-b-4 border-r-4">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black border-b-4 border-r-4">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black border-b-4">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black border-r-4">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black border-r-4">01</div>
-            <div class="cursor-pointer flex items-center justify-center border-black">01</div>
+        <div class="grid grid-cols-3 text-center w-60 h-60 mt-10">
+            <div class="flex cursor-pointer items-center justify-center border-r-4 border-b-4 border-black"></div>
+            <div class="cursor-pointer flex items-center justify-center border-r-4 border-b-4 border-black"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black border-b-4"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black border-b-4 border-r-4"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black border-b-4 border-r-4"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black border-b-4"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black border-r-4"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black border-r-4"></div>
+            <div class="cursor-pointer flex items-center justify-center border-black"></div>
         </div>
     </div>
       
